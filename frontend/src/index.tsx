df3dd5c24d8bc6f618c12fd632429143efeee502
paulclrt <p4ul.claret@gmail.com>
2025-05-21 19:31:15 +0200

    FIX: adding index.tsx with routing for loging

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Login";
+import Callback from "./Callback";
+import App from "./App"; // This will be the Dashboard page
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/callback" element={<Callback />} />
+      <Route path="/dashboard" element={<App />} />
+      <Route
+  path="/"
+  element={
+    localStorage.getItem("fitbit_access_token") ? (
+      <Navigate to="/dashboard" replace />
+    ) : (
+      <Navigate to="/login" replace />
+    )
+  }
+/>
+    </Routes>
+  </BrowserRouter>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
