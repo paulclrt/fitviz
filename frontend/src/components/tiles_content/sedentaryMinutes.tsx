4dc970c614952b7908d1d1112109ace043d498bc
paulclrt <p4ul.claret@gmail.com>
2025-05-30 19:10:37 +0200

    format sedentary minutes into humain redable data

--- frontend/src/components/tiles_content/sedentaryMinutes.tsx
+++ frontend/src/components/tiles_content/sedentaryMinutes.tsx
@@ -2,6 +2,22 @@ interface SedentaryProps {
   data: number | null
 }
 
+
+function FormatTimeData(data: number | null): string {
+    if (data === null) {
+        return "";
+    } else {
+        const hours: number = Math.floor(data / 60);
+        const minutes: number = Math.floor(data % 60);
+        if (minutes == 0) {
+            return `${hours}h`
+        } else {
+            return `${hours}h ${minutes}`
+        }
+    }
+}
+
+
 export function Sedentary({ data }: SedentaryProps) {
 
     if (data === null) {
@@ -13,7 +29,7 @@ export function Sedentary({ data }: SedentaryProps) {
     } else {
         return (
             <div className="flex flex-col items-center">
-            <h1 className="text-4xl text-gray-300">{data}</h1>
+            <h1 className="text-4xl text-gray-300">{FormatTimeData(data)}</h1>
             <p className="text-2xl">Sedentary Minutes</p>
             </div>
         )
