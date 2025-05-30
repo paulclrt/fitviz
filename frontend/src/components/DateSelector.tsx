e5fef55f51183d71766d8720cf81691bc01a92ae
paulclrt <p4ul.claret@gmail.com>
2025-05-30 19:24:07 +0200

    adding next and previous button for date selector

--- frontend/src/components/DateSelector.tsx
+++ frontend/src/components/DateSelector.tsx
@@ -5,14 +5,36 @@ interface DateSelectorProps {
   onChange: (newDate: string) => void;
 }
 
+
 export default function DateSelector({ selectedDate, onChange }: DateSelectorProps) {
+      const todayStr = new Date().toISOString().split("T")[0];
+
+    const dateBefore = () => {
+        const date = new Date(selectedDate);
+        date.setDate(date.getDate() - 1);
+        onChange(date.toISOString().split("T")[0]);
+    };
+
+  const dateAfter = () => {
+    const date = new Date(selectedDate);
+    date.setDate(date.getDate() + 1);
+    const newDate = date.toISOString().split("T")[0];
+    if (newDate <= todayStr) onChange(newDate);
+  };
+
+
+
   return (
+      <div>
+      <span className="px-2 cursor-pointer" onClick={dateBefore}>&lt;</span>
     <input
       type="date"
       value={selectedDate}
       onChange={(e) => onChange(e.target.value)}
       className="bg-transparent text-white border px-2 py-1 rounded"
     />
+      <span className="px-2 cursor-pointer" onClick={dateAfter}>&gt;</span>
+    </div>
   );
 }
 
